a515a03896784bcf8f9f57e29565d8bf276cdcbc
Stephen Bird <bird@deako.com>
2018-07-02 19:55:00 -0700

    Get react and typescript in here

---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string; }
+
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+export class Hello extends React.Component<HelloProps, {}> {
+    render() {
+        return <div>
+            hi
+        </div>;
+    }
+}
